12f268b0cabf4767a50e294ed09bb1a182933efd
Muhammad Saif <m.saifmumtaz@gmail.com>
2022-03-04 17:13:12 +0500

    Charts task completed

--- stats.php
+++ stats.php
@@ -1,51 +1,65 @@
 <?php
 require 'config/config.php';
 
+function sort_daily($ret){
+	$s=[];
+	foreach($ret as $r){
+		$s[$r['label']]=$r['total'];
+	}
+	for ($i = 0; $i < 6; $i++) {
+		$d=date('l', strtotime("-$i day"));
+		if(!array_key_exists($d,$s)){
+			$s[$d]=0;
+		}
+	 }
+
+	 $e=[];
+	 foreach($s as $d=>$v){
+		 $r=['label'=>$d,"total"=>$v];
+		 array_push($e,$r);
+	 }
+	 $dayOrder = [
+		'Saturday'  => 1,
+		'Sunday'    => 2,
+		'Monday'    => 3,
+		'Tuesday'   => 4,
+		'Wednesday' => 5,
+		'Thursday'  => 6,
+		'Friday'    => 7,
+	];
+	$ret=$e;
+	usort($ret,
+		function ($a, $b) use ($dayOrder) {
+			return $dayOrder[$a['label']] > $dayOrder[$b['label']];
+		});
+	return $ret;
+}
+
 if (isset($_SESSION['user_id']) && isset($_GET['type'])) {
 
-	$what = 'day AS label, hits AS total';
+	$what = 'DAYNAME(day) AS label, hits AS total';
 	$where = null;
 	$group = '';
 	$ret = [];
 
-	switch($_GET['type']){
-		case 'today':
-			$where = 'day = CURDATE()';
-		break;
-		case 'yesterday':
-			$where = 'day = subdate(current_date, 1)';
+	switch($_GET['type']){	
+		case 'daily':
+			$where = 'day >= SUBDATE(CURDATE(), INTERVAL 6 DAY) AND day<=CURDATE()';
 		break;	
-		case 'this_week':
-			$where = 'day >= SUBDATE(CURDATE(), dayofweek(CURDATE()) - 1)';
-		break;	
-		case 'last_week':
-			$where = 'YEARWEEK(day, 0) = YEARWEEK(CURDATE() - INTERVAL 1 WEEK, 0)';
+		case 'weekly':
+			$what = 'CONCAT("Week ", WEEK(day)," / ", YEAR(day)) AS label, SUM(hits) AS total';
+			$where = 'YEARWEEK(day, 0) >= YEARWEEK(CURDATE() - INTERVAL 4 WEEK, 0) AND YEARWEEK(DAY)<=YEARWEEK(CURDATE())';
+			$group = 'GROUP BY YEARWEEK(day)';
 		break;
-		case 'this_month':
-			$where = 'MONTH(day) = MONTH(CURRENT_DATE()) AND YEAR(day) = YEAR(CURRENT_DATE())';
-		break;
-		case 'last_month':
-			$where = 'MONTH(day) = MONTH(CURRENT_DATE() - INTERVAL 1 MONTH) AND YEAR(day) = YEAR(CURRENT_DATE() - INTERVAL 1 MONTH)';
-		break;
-		case 'last_3_months':
-			$what = 'MONTHNAME(day) AS label, SUM(hits) AS total';
-			$where = 'day >= LAST_DAY(NOW()) + INTERVAL 1 DAY - INTERVAL 3 MONTH';
-			$group = 'GROUP BY MONTH(day)';
-		break;
-		case 'last_6_months':
+		case 'monthly':
 			$what = 'MONTHNAME(day) AS label, SUM(hits) AS total';
 			$where = 'day >= LAST_DAY(NOW()) + INTERVAL 1 DAY - INTERVAL 6 MONTH';
 			$group = 'GROUP BY MONTH(day)';
 		break;
-		case 'this_year':
-			$what = 'MONTHNAME(day) AS label, SUM(hits) AS total';
-			$where = 'YEAR(day) = YEAR(CURRENT_DATE())';
-			$group = 'GROUP BY MONTH(day)';
-		break;
-		case 'last_year':
-			$what = 'DATE_FORMAT(day, "%M %Y") AS label, SUM(hits) AS total';
-			$where = 'YEAR(day) = YEAR(CURRENT_DATE() - INTERVAL 1 YEAR)';
-			$group = 'GROUP BY MONTH(day)';
+		case 'yearly':
+			$what = 'YEAR(day) AS label, SUM(hits) AS total';
+			$where = 'YEAR(day) >= YEAR(CURRENT_DATE() - INTERVAL 3 YEAR)';
+			$group = 'GROUP BY YEAR(day)';
 		break;
 	}
 
@@ -55,6 +69,13 @@ if (isset($_SESSION['user_id']) && isset($_GET['type'])) {
 		$ret = $stats -> fetchAll(PDO::FETCH_ASSOC);
 	}
 
+	switch($_GET['type']){
+		case 'daily':
+			$ret=sort_daily($ret);
+		break;	   
+	}
+	// print_r($ret);
+	// exit();
 	echo json_encode($ret);
 	
 }else {
--- test.php
+++ test.php
@@ -3,9 +3,13 @@ session_start();
 
 print_r($_SESSION);
 $months= [];
-for ($i = 0; $i < 6; $i++) {
-     array_push($months,date(', F', strtotime("-$i month")));
-  }
+$ddate = "2022-03-04";
+$date = new DateTime($ddate);
+$week = $date->format("W");
+echo "Weeknummer: $week";
+// for ($i = 0; $i < 6; $i++) {
+//      array_push($months,date('F', strtotime("-$i Month")));
+//   }
 
-$dates = array_reverse($months);
-print_r($dates);
+// $dates = array_reverse($months);
+// print_r($dates);
