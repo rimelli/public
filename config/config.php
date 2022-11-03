e9acb11317c2433a306a40c32badf626d4c95f8e
Ricardo Lima <rml12588@gmail.com>
2022-11-03 12:27:25 +0000

    upwork training

--- config/config.php
+++ config/config.php
@@ -11,12 +11,4 @@ $dbname = "upwork";
 
 $con = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 
-// Pusher Credentials
-
-$cluster = 'ap2';
-$auth_key = 'e3b4cff89826a7755f9f';
-$secret = '3b5cb8ce69d9dbba7910';
-$app_id = '1478386';
-$chanel_name = 'football-app'
-
 ?>
